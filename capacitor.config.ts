0f1ad6bed7f95b0d07eb7d65859ea2d540d64b2a
blaicb99 <blaicb99@gmail.com>
2025-12-03 14:02:26 +0000

    Add Capacitor Android and configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.elteunom.calendariadvent',
+  appName: 'calendari-advent',
+  webDir: 'dist'
+};
+
+export default config;
